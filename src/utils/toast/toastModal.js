e167e6ff8c344187d71db06479ba51bd9b53e0bb
Adi <121740959+Adi020@users.noreply.github.com>
2024-07-21 16:24:08 -0600

    Add files via upload

---
+++ src/utils/toast/toastModal.js
@@ -0,0 +1,49 @@
+import { toast, Zoom } from "react-toastify";
+
+export const toastsuccess = (message, toastId, autoClose) => {
+  toast.success(message, {
+    toastId,
+    autoClose,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    transition: Zoom,
+  });
+};
+
+export const toastError = (message, toastId) => {
+  toast.error(message, {
+    toastId,
+    position: "top-center",
+    autoClose: 1000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const toastPromise = (promise, pending, toastId, delay) => {
+  toast.promise(
+    promise,
+    {
+      pending,
+      success: "Successfully brought",
+      error: "An error occurred, please try again later.",
+    },
+    {
+      toastId,
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      transition: Zoom,
+      delay: delay || 0
+    }
+  );
+};
